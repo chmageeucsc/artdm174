a11eb3e2154bff8ad73d74f48cbb57bb07e88d0c
Chantel <chmagee@ucsc.edu>
2024-09-20 14:36:34 -0700

    fixed js file, need mouse position display

--- Labs/lab_3/lab3.js
+++ Labs/lab_3/lab3.js
@@ -3,7 +3,43 @@ function playSong() {
 };
 
 // Access the button
-document.getElementById("play").addEventListener('click',function ()
+window.addEventListener('load',function(){
+  document.getElementById("play").addEventListener('click',function ()
   {
     playSong();
   }  ); 
+
+  // Return the name of the mouse button
+  const getMouseButton = code => {
+    let button = "unknown";
+    switch (code) {
+      case 0: // 0 is the code for the left mouse button
+        button = "left";
+        break;
+      case 1: // 1 is the code for the middle mouse button
+        button = "middle";
+        break;
+      case 2: // 2 is the code for the right button
+        button = "right";
+        break;
+    }
+    return button;
+  };
+
+  // Show info about mouse event
+  const mouseInfo = e => {
+    console.log(
+      `Mouse event: ${e.type}, button: ${getMouseButton(
+        e.button
+      )}, X: ${e.clientX}, Y: ${e.clientY}`
+    );
+  };
+
+  // Add mouse click event listener
+  document.addEventListener("click", mouseInfo);
+
+  const paragraph = document.getElementById("my-paragraph");
+  paragraph.textContent = "New text here";
+
+
+});
