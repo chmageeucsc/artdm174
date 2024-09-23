dda95f9aa0688b69d56a795a269eec3850b94dfd
Chantel <chmagee@ucsc.edu>
2024-09-22 18:14:48 -0700

    displays mouse position

--- Labs/lab_3/lab3.js
+++ Labs/lab_3/lab3.js
@@ -1,45 +1,10 @@
-function playSong() {
-  alert("Play song!");
-};
+function position(event) {
+  let x = event.clientX;
+  let y = event.clientY;
+  document.getElementById("X").value = x;
+  document.getElementById("Y").value = y;
+}
 
-// Access the button
 window.addEventListener('load',function(){
-  document.getElementById("play").addEventListener('click',function ()
-  {
-    playSong();
-  }  ); 
-
-  // Return the name of the mouse button
-  const getMouseButton = code => {
-    let button = "unknown";
-    switch (code) {
-      case 0: // 0 is the code for the left mouse button
-        button = "left";
-        break;
-      case 1: // 1 is the code for the middle mouse button
-        button = "middle";
-        break;
-      case 2: // 2 is the code for the right button
-        button = "right";
-        break;
-    }
-    return button;
-  };
-
-  // Show info about mouse event
-  const mouseInfo = e => {
-    console.log(
-      `Mouse event: ${e.type}, button: ${getMouseButton(
-        e.button
-      )}, X: ${e.clientX}, Y: ${e.clientY}`
-    );
-  };
-
-  // Add mouse click event listener
-  document.addEventListener("click", mouseInfo);
-
-  const paragraph = document.getElementById("my-paragraph");
-  paragraph.textContent = "New text here";
-
-
+  
 });
