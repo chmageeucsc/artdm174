7781816c831fdddf8f55e9124bc2dfe66068a2e1
Chantel <chmagee@ucsc.edu>
2024-10-04 12:04:18 -0700

    desktop and mobile javascript fixed

--- Labs/lab_4/lab4.js
+++ Labs/lab_4/lab4.js
@@ -5,20 +5,33 @@
 document.addEventListener("DOMContentLoaded", initSlider);
 document.querySelector(".grid").style.display = "none";
 document.querySelector(".wJS").style.display = "block";
-document.querySelector(".prev").style.display = "block";
-document.querySelector(".next").style.display = "block";
 
-// FIX MOBILE VIEW
-
-// if (window.screen.innerWidth < 700 ) {
-//   document.querySelector(".prev").style.display = "none";
-//   document.querySelector(".next").style.display = "none";
-// }
+setInterval(function() {
+  if (window.innerWidth >= 700 ) {
+    document.querySelector(".prevMobile").style.display = "none";
+    document.querySelector(".nextMobile").style.display = "none";
+    document.querySelector(".buttonsMobile").style.display = "none";
+    document.querySelector(".prev").style.display = "block";
+    document.querySelector(".next").style.display = "block";
+  }
+  
+  if (window.innerWidth < 700 ) {
+    document.querySelector(".prev").style.display = "none";
+    document.querySelector(".next").style.display = "none";
+    document.querySelector(".buttonsMobile").style.display = "flex";
+    document.querySelector(".prevMobile").style.display = "block";
+    document.querySelector(".nextMobile").style.display = "block";
+  }
+}, 1000);
 
 function initSlider() {
 
+  // desktop
   const back_btn = document.querySelector(".prev");
   const next_btn = document.querySelector(".next");
+  // mobile
+  const backMob = document.querySelector(".prevMobile");
+  const nextMob = document.querySelector(".nextMobile");
   const slides = document.querySelector(".slides");
   const slide = slides.querySelectorAll("img");
 
@@ -30,8 +43,12 @@ function initSlider() {
   // show first slide
   slide[0].classList.remove("hide");
 
+  // desktop
   next_btn.addEventListener("click",changeSlide);
   back_btn.addEventListener("click", changeSlide);
+  // mobile
+  nextMob.addEventListener("click",changeSlide);
+  backMob.addEventListener("click", changeSlide);
 
   const alt = slides[0].getAttribute('alt');
   // Get the figcaption element
@@ -47,11 +64,19 @@ function changeSlide(e) {
   let showing = document.querySelector(".current");
   let nextUp = "";
 
-  // check which button was pressed
+  // check which button was pressed DESKTOP
   if(e.target.className == "prev") {
     nextUp = showing.previousElementSibling;
   }
   if(e.target.className == "next") {
+    nextUp = showing.nextElementSibling;
+  }
+
+  // check which button was pressed MOBILE
+  if(e.target.className == "prevMobile") {
+    nextUp = showing.previousElementSibling;
+  }
+  if(e.target.className == "nextMobile") {
     nextUp = showing.nextElementSibling;
   }
 
