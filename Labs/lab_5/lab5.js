b9d05a20d6bf9a998f9f600337148515dc43d9ca
Chantel <chmagee@ucsc.edu>
2024-10-21 15:45:28 -0700

    added custom music and video, added captions

--- Labs/lab_5/lab5.js
+++ Labs/lab_5/lab5.js
@@ -1,56 +1,64 @@
-// copy of Lab 5a Basic Audio/Video Repo
+// Student Name: Chantel Gee
+// Assignment Name: Lab 5: Audio and Video
+// Date: 10.21.2024
 
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    /**
-     * Shortcut variables
-     */
-    const lowRider = document.querySelector(".low-rider");
-    const leverage = document.querySelector(".leverage");
-    const ff = document.getElementById("ff");
-    const slo = document.getElementById("slo");
-    const normal = document.getElementById("normal");
-    const pick = document.getElementById("pick");
+  /**
+   * Shortcut variables
+   */
+  const shopChannel = document.querySelector(".shop-channel");
+  const teddy = document.querySelector(".teddy");
+  const ff = document.getElementById("ff");
+  const slo = document.getElementById("slo");
+  const normal = document.getElementById("normal");
+  const pick = document.getElementById("pick");
 
-    /**
-     * prepare the audio and video for playing
-     */
-    lowRider.src = "audio/money.mp3";
-    lowRider.load();
-    lowRider.volume = 0.5;
+  /**
+   * prepare the audio and video for playing
+   */
+  shopChannel.src = "audio/MiiPlaza.mp3";
+  shopChannel.load();
+  shopChannel.volume = 0.5;
 
-    //set video's initial volume
-    leverage.volume = 0.5;
+  //set video's initial volume
+  teddy.volume = 0.5;
 
-    /**
-     * create the button event listeners to control the audio
-     */
+  /**
+   * create the button event listeners to control the audio
+   */
 
-    ff.addEventListener("click", (e) => {
-        lowRider.playbackRate = 2;
-    });
+  ff.addEventListener("click", (e) => {
+    shopChannel.playbackRate = 2;
+  });
 
-    slo.addEventListener("click", (e) => {
-        lowRider.playbackRate = 0.5;
-    });
+  slo.addEventListener("click", (e) => {
+    shopChannel.playbackRate = 0.5;
+  });
 
-    normal.addEventListener("click", (e) => {
-        lowRider.playbackRate = 1;
-    });
+  normal.addEventListener("click", (e) => {
+    shopChannel.playbackRate = 1;
+  });
 
-    /**
-     * select lists emit a "change" event when the choice is changed
-     */
-    pick.addEventListener("change", (e) => {
-        // save the audio's current place in the song.
-        let time = lowRider.currentTime;
+  /**
+   * select lists emit a "change" event when the choice is changed
+   */
+  pick.addEventListener("change", (e) => {
+    // save the audio's current place in the song.
+    let time = shopChannel.currentTime;
 
-        lowRider.src = e.target.value;
-        lowRider.load();
-        lowRider.play();
+    shopChannel.src = e.target.value;
+    shopChannel.load();
+    shopChannel.play();
 
-        // set the new song to the same place as the previous one.
-        lowRider.currentTime = time;
-    });
+    // set the new song to the same place as the previous one.
+    shopChannel.currentTime = time;
+  });
+
+  let textTrackElem = document.getElementById("text-track");
+
+  textTrackElem.addEventListener("cuechange", (event) => {
+    let cues = event.target.track.activeCues;
+  });
 } // end init function
