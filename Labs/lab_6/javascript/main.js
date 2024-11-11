e301be4fccb76b27af8708f91f4b6113b863d369
Chantel <chmagee@ucsc.edu>
2024-11-10 17:06:14 -0800

    used async and await functions

--- Labs/lab_6/javascript/main.js
+++ Labs/lab_6/javascript/main.js
@@ -1,36 +1,16 @@
 
+//
+// Student Name: Chantel Gee
+// Assignment Name: Lab 6: Fetch
+// Date: 11.10.2024
+//
+
 //This code does NOT create any global variables.
 //Promises can be chained together, with the previous promise
 // passing its results to the next one in the chain.
 // the format is: fetch().then().then().catch()
 //it's easier to read if we put each step in its own line,
 //that's why the periods start the then lines.
-
-fetch("houses.json")
-    .then((response) => response.json())
-    .then((data) => {
-        //create a temp holder to append all the html generated inside the forEach iterator
-        let html = "";
-
-        //the argument "house" passed to the arrow function
-        //holds each item in the array in turn.
-        data.forEach((house) => {
-            let family = house.members.join(" | ");
-
-            // generate the html snippet for one array item
-            //to be added to the "html" temp holder.
-            let objInfo = `<dt class="house">${house.name}</dt>
-            <dd class="folks">${family}</dd>`;
-            html += objInfo;
-        });
-
-        //make a reference to the html container where
-        //the info will be displayed.
-        const container = document.querySelector("#container");
-        container.innerHTML = html;
-    })
-    .catch((err) => console.log("Oops!", err));
-    //this only runs if there is an error during the above process
 
 // random color for background
 function randomHex() {
@@ -49,12 +29,44 @@ function randomHex() {
     return(color);
 }
 
-fetch("https://www.thecolorapi.com/id?hex=" + randomHex() + "&format=json")
-    .then((response) => response.json())
-    .then((data) => {
-        document.body.style.background = data.hex.value;
-        console.log(data.hex.value);
-    })
-    .catch((err) => console.log("Colr error!", err));
+async function loadHouses() {
+    return (await fetch("houses.json")).json();
+}
 
+async function loadColor() {
+    return (await fetch("https://www.thecolorapi.com/id?hex=" + randomHex() + "&format=json")).json();
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    let houses = [];
+
+    try {
+        houses = await loadHouses();
+        color = await loadColor();
+    } catch (e) {
+        console.log("Error!");
+        console.log(e);
+    }
+
+    let html = "";
+
+    houses.forEach((house) => {
+        let family = house.members.join(" | ");
+
+        // generate the html snippet for one array item
+        //to be added to the "html" temp holder.
+        let objInfo = `<dt class="house">${house.name}</dt>
+        <dd class="folks">${family}</dd>`;
+        html += objInfo;
+    });
+
+    //make a reference to the html container where
+    //the info will be displayed.
+    const container = document.querySelector("#container");
+    container.innerHTML = html;
+    
+    // console.log(houses);
+
+    document.body.style.background = color.hex.value;
+});
     
