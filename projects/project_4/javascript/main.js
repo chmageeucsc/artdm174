ab9ee1a4a342bebce958f0a678d2570932c7206c
Chantel <chmagee@ucsc.edu>
2024-12-09 12:31:11 -0800

    async await. tentative use of shuffle

--- projects/project_4/javascript/main.js
+++ projects/project_4/javascript/main.js
@@ -5,44 +5,56 @@
 // Date: 12.09.2024
 //
 
-let deckID = '';
+// temp deckID
+let deckID = '8cjiymvkscng';
 let drawCardURL1 = 'https://deckofcardsapi.com/api/deck/'
 let drawCardURL2 = '/draw/?count=1'
 
-function getDeck() {
-  fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-      deckID = Object.values(data)[1];
-      console.log("first: " + deckID);
 
-      playGame(deckID)
-      // console.log(Object.values(data)[1])
-    }
-  )
-  .catch(error => console.log("ERROR"))
+// async function getDeck() {
+//   return (await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")).json();
+// }
+
+async function getCard() {
+  return (await fetch(drawCardURL1 + deckID + drawCardURL2)).json();
 }
 
-getDeck();
-
-function playGame(ID) {
-  let playerHand = [];
-  let dealerHand = [];
-  getCard(ID);
-
+async function shuffleDeck() {
+  return (await fetch("https://deckofcardsapi.com/api/deck/" + deckID + "/shuffle/?remaining=true ")).json();
 }
 
-function getCard(ID) {
-  fetch(drawCardURL1 + ID + drawCardURL2)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-      console.log(Object.values(data)[2][0].suit);
-      console.log(Object.values(data)[2][0].value);
-    }
-  )
-  .catch(error => console.log("ERROR"))
-}
+document.addEventListener("DOMContentLoaded", async () => {
+  let deck = [];
+
+  try {
+      // deck = await getDeck();
+      newCard = await getCard();
+      await shuffleDeck();
+  } catch (e) {
+      console.log("Error!");
+      console.log(e);
+  }
+
+  // console.log(Object.values(deck)[1]);
+  // deckID = Object.values(deck)[1];
+  // console.log(deckID);
+  newCard = await getCard();
+  console.log(Object.values(newCard)[2][0].value);
+  await shuffleDeck();
+  newCard = await getCard();
+  console.log(Object.values(newCard)[2][0].value);
+  await shuffleDeck();
+  newCard = await getCard();
+  console.log(Object.values(newCard)[2][0].value);
+  await shuffleDeck();
+  newCard = await getCard();
+  console.log(Object.values(newCard)[2][0].value);
+  await shuffleDeck();
+  newCard = await getCard();
+  console.log(Object.values(newCard)[2][0].value);
+  await shuffleDeck();
+  newCard = await getCard();
+  console.log(Object.values(newCard)[2][0].value);
+
+
+});
