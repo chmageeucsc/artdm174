7501eab0540239fd9fc83fccd2b1ab6bc899bcc8
Chantel <chmagee@ucsc.edu>
2024-12-09 10:26:57 -0800

    deck ID works

--- projects/project_4/javascript/main.js
+++ projects/project_4/javascript/main.js
@@ -5,3 +5,21 @@
 // Date: 12.09.2024
 //
 
+let deckID = '';
+let drawCardURL1 = 'https://deckofcardsapi.com/api/deck/'
+let drawCardURL2 = '/draw/?count=1'
+
+function getDeck() {
+  fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => 
+    // deckID = Object.values(data)[1]
+    console.log(Object.values(data)[1])
+    //document.getElementById("catFax").innerHTML = Object.values(data)[0]
+  )
+  .catch(error => console.log("ERROR"))
+}
+
+getDeck();
