1fd134f6c8f82c90fdc208f74fe3d88cbad0f1da
Chantel Gee <chmagee@ucsc.edu>
2025-01-31 16:01:07 -0800

    adjustments to main page

---
+++ javascript/main.js
@@ -0,0 +1,20 @@
+//
+// Student Name: Chantel Gee
+// Assignment Name: Main Page
+// Date: 12.11.2024
+//
+
+// const title173 = document.getElementById("title173");
+// const artdm173 = document.getElementById("artdm173");
+const title174 = document.getElementById("title174");
+const artdm174 = document.getElementById("artdm174");
+
+// // show and hide class assignments
+// title173.addEventListener("click", (e) => {
+//   artdm173.classList.toggle("hide");
+// });
+
+// show and hide class assignments
+title174.addEventListener("click", (e) => {
+  artdm174.classList.toggle("hide");
+}); 
